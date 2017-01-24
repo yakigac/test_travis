76efaeaa7555af5e0a8e38ea840368213f502389
Ryohei Kuroki <ryohei.kuroki@gmail.com>
2017-01-24 23:43:30 +0900

    Update cpp to use opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <opencv2/core/core.hpp>
 #include <iostream>
 
 int main(){
     std::cout << "hello world!" << std::endl;
+
+    cv::Mat mat = (cv::Mat_<double>(3, 3) << 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
     return 0;
 }
