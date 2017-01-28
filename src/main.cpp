9b079f878b55ecbe5ce42455f7f3c0f3d6ab8cb5
Ryohei Kuroki <ryohei.kuroki@gmail.com>
2017-01-28 18:27:58 +0900

    Add parameters for using boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 
-int main(){
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[]){
     std::cout << "hello world!" << std::endl;
+
+
+    po::options_description desc("Allowed options");
+
+	desc.add_options()
+		("help, a", "produce help message")
+		("greet, b", po::value<std::string>(), "hi")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help")) {
+		std::cout << desc << std::endl;
+
+		return 0;
+	}
+
     return 0;
 }
